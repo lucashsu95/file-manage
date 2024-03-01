39b767f7388d376ec98adf3b46387d4e9c186890
lucashsu95 <lucashsu95@gmail.com>
2024-03-01 09:21:49 +0800

    文件 -> Word

--- main/after.py
+++ main/after.py
@@ -5,7 +5,7 @@ def after_files():
     # 定義檔案擴展名和對應的類別
     file_extensions = {
         'pdf': 'PDF', 'png': '圖片', 'jpg': '圖片', 'jpeg': '圖片', 'gif': '圖片',
-        'doc': '文件', 'docx': '文件', 'csv': '文件', 'xlsx': 'Excel', 'pptx': 'PowerPoint',
+        'doc': 'Word', 'docx': 'Word', 'csv': 'Word', 'xlsx': 'Excel', 'pptx': 'PowerPoint',
         'ini': '設定檔', 'txt': '文字檔', 'srt': '文字檔', 'zip': '壓縮檔', 'rar': '壓縮檔',
         'exe': '執行檔', 'wav': '音樂', 'mp3': '音樂', 'mp4': '影片', 'avi': '影片',
         'flv': '影片', 'wmv': '影片', 'webm': '影片', 'ai': 'Illustrator', 'psd': 'Photoshop',
